dd18ed233ec3cf2b1ee7a8faa78733a0599f1b3a
shakedlokits <shaked.lokits@gmail.com>
2016-05-23 15:12:13 +0300

    added debug mode

--- pocket_server.py
+++ pocket_server.py
@@ -33,5 +33,5 @@ def testing_conn():
     return "Everything is within normal parameters captain"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='8080')
+    app.run(host='0.0.0.0', port='8080', debug=True)
 
